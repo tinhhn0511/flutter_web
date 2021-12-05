c14e6eff431f65ffcee327fc1c29bb2cdfbf29bd
tinhh0511 <=>
2021-12-05 15:14:32 +0700

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter_web/widgets/bottom_bar.dart';
+import 'package:flutter_web/widgets/carousel.dart';
+import 'package:flutter_web/widgets/featured_heading.dart';
+import 'package:flutter_web/widgets/featured_tiles.dart';
+import 'package:flutter_web/widgets/floating_quick_access_bar.dart';
+import 'package:flutter_web/widgets/main_heading.dart';
+import 'package:flutter_web/widgets/menu_drawer.dart';
+import 'package:flutter_web/widgets/top_bar_contents.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ScrollController _scrollController = ScrollController();
+  double _scrollPosition = 0;
+  double _opacity = 0; // dộ mờ
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40 // tính năng cuộc thanh menu
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
+      debugPrint(_opacity.toString());
+    return Scaffold(
+      extendBodyBehindAppBar: true, // beautifull task bar
+      appBar: PreferredSize( // kích thước ưa chuộng
+        preferredSize: Size(screenSize.width, 70), //rong
+        child: TopBarContents(_opacity), // cao
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  child: SizedBox(
+                    height: screenSize.height * 0.65,
+                    width: screenSize.width,
+                    child: Image.asset(
+                      'assets/images/background.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+
+                    FloatingQuickAccessBar(screenSize: screenSize),
+                    FeaturedHeading(screenSize: screenSize),
+                    FeaturedTiles(screenSize: screenSize),
+                    MainHeading(screenSize: screenSize),
+                    MainCarousel(),
+                    SizedBox(height: screenSize.height / 10),
+                    BottomBar(),
+
+                  ],
+                  )
+                
+              ],
+            ),
+
+          ],
+        )
+        ),
+
+    );
+  }
+}
---
+++ lib/widgets/bottom_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_web/widgets/bottom_bar_column.dart';
+import 'package:flutter_web/widgets/info_text.dart';
+import 'package:flutter_web/widgets/responsive.dart';
+import 'package:flutter/material.dart';
+//thanh footter
+class BottomBar extends StatelessWidget {
+  const BottomBar({
+    Key? key,
+  }) : super(key: key);
+  static const Color gradientStartColor = Color(0xff11998e);
+  static const Color gradientEndColor = Color(0xff0575E6);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(0.0)),
+          gradient: LinearGradient(
+              colors: [
+                gradientStartColor,
+                gradientEndColor
+              ],
+              begin: const FractionalOffset(0.2, 0.2),
+              end: const FractionalOffset(1.0, 1.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+        ),
+      padding: EdgeInsets.all(30),
+      //color: Colors.blueGrey[900],
+      child: Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    BottomBarColumn(
+                      heading: 'ABOUT',
+                      s1: 'Contact Us',
+                      s2: 'About Us',
+                      s3: 'Careers',
+                    ),
+                    Container(
+                      color: Colors.white,
+                      width: 2,
+                      height: 150,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InfoText(
+                          type: 'Email',
+                          text: 'tinhhn0511@gmail.com',
+                        ),
+                        SizedBox(height: 5),
+                        InfoText(
+                          type: 'Address',
+                          text: 'HO CHI MINH CITY',
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Colors.white,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Designed by gr PRIVATE @2021',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
